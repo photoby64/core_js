d0c3778892f2911d9e379af37030aaeb8ee13cc5
photoby64 <photoby64@naver.com>
2024-10-30 09:44:55 +0900

    변수와 상수 문제풀이

--- client/chapter/core/02.variable.js
+++ client/chapter/core/02.variable.js
@@ -18,13 +18,34 @@ let currentUserName = "lazy64";
 /* 다음 내용을 분석한 후, 프로그래밍 할 수 있도록 변수와 상수를 작성해봅니다. ----------- */
 
 // - 갯수 별 상품 가격 계산하기
+let productsByQuantity = 1000;
+
+
 // - 구매 제품 가격의 총 합
-// - 1년 기준 일(day)자 수
+let totalProductPrice = 10000;
+
+
+
+
 // - 구매 결제 여부
+let isPayment = true;
+let hasCartItem = false;
+
+
 // - 구매 결제 내역
+let paymentList;
+
+
 // - 브랜드 접두사
+const brand_prefix = 'Apple';
+
+
 // - 오늘의 운세
-// - 상품 정보
+let todaysFortune = 'lucky day'
+let fortuneToday = 'good'
+
+
+
 
 
 /* variables ----------------------------------------------------------- */
@@ -32,3 +53,12 @@ let currentUserName = "lazy64";
 
 /* constant variables -------------------------------------------------- */
 
+
+
+
+// - 1년 기준 일(day)자 수
+const DAY_PER_YEAR = 365;
+
+
+// - 상품 정보
+const productInfo = '컵'
