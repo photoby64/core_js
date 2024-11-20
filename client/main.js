641240e8ee4f4ca2d6f8277202b3499fd7d41068
photoby64 <photoby64@naver.com>
2024-11-20 14:38:17 +0900

    3

--- client/main.js
+++ client/main.js
@@ -6,9 +6,7 @@ import {
 
 
 // diceAnimation()
-
 // setInterval
-
 // 주사위 굴리기 버튼을 누르면 주사위가 애니메이션이 될 수있도록
 
 
@@ -16,19 +14,24 @@ const [rollingButton, recordButton, resetButton] = getNodes('.buttonGroup > butt
 
 
 
-const handleRollingDice = () => {
+const handleRollingDice = (() => {
   let isClicked = false;
   let stopAnimation;
 
   return () => {
       if (!isClicked) {
           stopAnimation = setInterval(diceAnimation, 100);
+          recordButton.disabled = true;
+          resetButton.disabled = true;
+          
+
       } else {
           clearInterval(stopAnimation);
+          recordButton.disabled = false;
+          resetButton.disabled = false;
       }
       isClicked = !isClicked;
   };
-};
+})();
 
-
-rollingButton.addEventListener('click',handlerRollingDice)
+rollingButton.addEventListener('click', handleRollingDice);
