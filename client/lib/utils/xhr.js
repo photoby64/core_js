5f3c4c7c80db946c409b86b9b9e9e72921cd4de9
photoby64 <photoby64@naver.com>
2024-11-21 11:22:38 +0900

    3

--- client/lib/utils/xhr.js
+++ client/lib/utils/xhr.js
@@ -1,5 +1,4 @@
 
-const END_POINT= 'https://jsonplaceholder.typicode.com/users';
 
 
 // [readyState]
@@ -11,10 +10,30 @@ const END_POINT= 'https://jsonplaceholder.typicode.com/users';
 
 // 0:아무일도없음 1:로딩중 2:로딩이완료된상태 3:interactive 4:완료 
 
-const xhr = new XMLHttpRequest();
+// Endpoint(엔드포인트)**는 일반적으로 API 요청을 보낼 대상 URL을 의미
+
+// 콜백은 인수로 함수를 던지는것. 안에 있는 데이터를 꺼낼때
+
+
+
+
+const END_POINT= 'https://jsonplaceholder.typicode.com/users';
+
+
+
+function xhr(method,url,callback){
+
+  const xhr = new XMLHttpRequest();
 // console.log(xhr)
 
 xhr.open('GET',END_POINT);
+
+
+
+
+
+
+
 
 xhr.addEventListener('readystatechange', ()=>{
 
@@ -23,9 +42,12 @@ xhr.addEventListener('readystatechange', ()=>{
     if(xhr.status >= 200 && xhr.status < 400){
       // console.log(JSON.parse(xhr.response));
       const data = JSON.parse(xhr.response);
+      callback(data)
 
+      
     }else{
-      console.log('실패');
+      // console.log('실패');
+    fail({message:'알수없는 오류가 발생'})
     }
   }
   //console.log(xhr.readyState);
@@ -35,3 +57,14 @@ xhr.addEventListener('readystatechange', ()=>{
 
 xhr.send();
 
+
+}
+
+xhr('GET', END_POINT,(callback)=>{ 
+  console.log(callback);
+
+},(error)=>{
+  console.log(err.message);
+
+
+})
