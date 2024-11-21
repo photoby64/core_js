99718e5afd4bde9fda7853aaed5ddcb3779a8b9d
photoby64 <photoby64@naver.com>
2024-11-21 14:20:25 +0900

    Promise 1

--- client/lib/utils/delay.js
+++ client/lib/utils/delay.js
@@ -17,21 +17,40 @@ const first= getNode('.first');
 const second = getNode('.second');
 
 
-delay(()=>{
-  first.style.top = '-100px'
-  second.style.top = '100px'
+// delay(()=>{
+//   first.style.top = '-100px'
+//   second.style.top = '100px'
 
-  delay(()=>{
-    first.style.transform = 'rotate(360deg)'
-    second.style.transform = 'rotate(360deg)'
+//   delay(()=>{
+//     first.style.transform = 'rotate(360deg)'
+//     second.style.transform = 'rotate(360deg)'
 
-    delay(()=>{
-      first.style.top = '0px'
-      second.style.top = '0px'
+//     delay(()=>{
+//       first.style.top = '0px'
+//       second.style.top = '0px'
   
-    })
+//     })
   
-  })
+//   })
+
+// })
+
+
+const shouldRejected= false;
+
+
+const p =  new Promise((성공,실패)=>{
+  if(!shouldRejected){
+    성공('성공입니다~')
+  }else{
+    실패('실패입니다~')
+  }
+
 
 })
+console.log(p);
 
+p.then((res)=>{
+  console.log(res);
+
+})
