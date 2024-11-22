373f6fe972d25c40f53696b1aa656ff0a8153d11
photoby64 <photoby64@naver.com>
2024-11-22 16:47:30 +0900

    복습 파일 생성

---
+++ client/_main.js
@@ -0,0 +1,110 @@
+
+
+import { 
+  tiger,
+  delayP, 
+  getNode,
+  insertLast, 
+  changeColor,
+  renderSpinner,
+  renderUserCard,
+  renderEmptyCard
+ } from "./lib/index.js";
+
+
+
+const END_POINT = 'https://jsonplaceholder.typicode.com/users'
+
+
+const userCardInner = getNode('.user-card-inner');
+
+
+
+renderSpinner(userCardInner)
+
+
+
+async function renderUserList(){
+
+
+  try{
+
+    const response = await tiger.get(END_POINT);
+
+    // getNode('.loadingSpinner').remove()
+
+    gsap.to('.loadingSpinner',{
+      opacity:0,
+      onComplete(){
+        
+        this._targets[0].remove()
+      }
+    })
+
+    const data = response.data;
+  
+    await delayP(1000)
+  
+    
+    data.forEach((user)=> renderUserCard(userCardInner,user))
+  
+    changeColor('.user-card');
+  
+    gsap.from('.user-card',{
+      // delay:1,
+      x:-100,
+      opacity:0,
+      stagger:{
+        amount:1,
+        from:'start'
+      }
+    })
+  
+  
+  }
+  catch{
+
+    renderEmptyCard(userCardInner)
+    console.log('error!');
+    
+  }
+
+}
+
+
+
+renderUserList()
+
+
+
+
+
+// 1. user 데이터 fetch 해주세요.
+
+
+// 2. fetch 데이터 유저의 이름만 콘솔에 출력 
+
+
+
+function handleDeleteCard(e){
+  const button = e.target.closest('button');
+
+  if(!button) return;
+
+  const article = button.parentElement;
+  const index = article.dataset.index.slice(5);
+  tiger.delete(`${END_POINT}/${index}`).then(()=>{
+    alert('삭제가 완료됐습니다.'); // 핸들러는 비동기 함수로 async로 만들지 않음. than을 쓰자
+  })
+
+
+
+  console.log();
+  
+
+
+}
+
+
+
+userCardInner.addEventListener('click', handleDeleteCard);
