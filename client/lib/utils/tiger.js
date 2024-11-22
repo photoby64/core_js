e8da8cf54da3851a97b86196b57cba3b6244125e
photoby64 <photoby64@naver.com>
2024-11-22 11:43:57 +0900

    7

--- client/lib/utils/tiger.js
+++ client/lib/utils/tiger.js
@@ -12,32 +12,114 @@ const defaultOptions = {
 
 const tiger = async (options) =>{
 
-  const { url, ...restOptions } = {
-    ...defaultOptions,       // 기본 옵션
-    ...options,              // 사용자 정의 옵션
-    headers: {
-      ...defaultOptions.headers, // 기본 헤더
-      ...options.headers,        // 사용자 정의 헤더
-    },
-  };
+  const {url,...rest} = {
+    ...defaultOptions,
+    ...options,
+    headers:{
+      ...defaultOptions.headers,
+      ...options.headers
+    }
+  }
 
-  const response = await fetch(url, restOptions); // HTTP 요청
+  const response = await fetch(url,rest);
 
   if(response.ok){
     response.data = await response.json();
+  }
 
-  }  
   return response
 }
 
-const response = await tiger({ url: END_POINT});
 
-console.log(response.data);
-
+// const response = await tiger({ url: END_POINT });
 
 
+tiger.get = (url,options) => {
+  return tiger({
+    url,
+    ...options
+  })
+}
 
 
-// console.log(await response.json());
+tiger.post = (url,body,options) => {
+  return tiger({
+    method:'POST',
+    url,
+    ...options,
+    body:JSON.stringify(body),
+  })
+}
+
+tiger.put = (url,body,options) => {
+  return tiger({
+    method:'PUT',
+    url,
+    ...options,
+    body:JSON.stringify(body),
+  })
+}
+
+tiger.patch = (url,body,options) => {
+  return tiger({
+    method:'PATCH',
+    url,
+    ...options,
+    body:JSON.stringify(body),
+  })
+}
+
+tiger.delete = (url,options) => {
+  return tiger({
+    method:'DELETE',
+    url,
+    ...options,
+  })
+}
 
 
+
+
+
+
+// tiger.get()
+// tiger.put()
+// tiger.delete()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function getGeolocation(){
+
+  return new Promise((resolve, reject) => {
+
+    navigator.geolocation.getCurrentPosition((data)=>{
+      const {latitude:lat, longitude:long} = data.coords;
+      resolve({ lat, long })
+    })  
+
+  })
+
+}
+
+
+const data = await getGeolocation();
+
+// console.log( data );
