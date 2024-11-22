7e5f533cc85704c5e896b9e635f728e881ac2860
photoby64 <photoby64@naver.com>
2024-11-22 14:04:08 +0900

    태그를 생성하는 함수

--- client/lib/dom/userList.js
+++ client/lib/dom/userList.js
@@ -1,9 +1,3 @@
-import { 
-  tiger, 
-  getNode,
-  insertLast, 
- } from "./lib/index.js";
-
 
 
 const END_POINT = 'https://jsonplaceholder.typicode.com/users'
@@ -47,7 +41,33 @@ renderUserList()
 
 
 
-// 1. user 데이터 fetch 해주세요.
+
+// 1. 태그를 생성하는 함수
 
 
-// 2. fetch 데이터 유저의 이름만 콘솔에 출력 
+
+function createUserCard({
+  username = 'unknown',
+  email = 'unknown@unknown.com',
+  website = 'unknown.co.kr'
+}){
+
+  return `
+    <article class="user-card" data-index="user-1">
+      <h3 class="user-name">${username}</h3>
+      <div class="user-resouce-info">
+        
+        <div>
+          <a class="user-email" href="mailto:${email}">${email}</a>
+        </div>
+        <div>
+          <a class="user-website" href="http://${website}" target="_blank" rel="noopener noreferer">${website}</a>
+        </div>
+      </div>
+      <button class="delete">삭제</button>
+    </article>
+  `
+}
+
+
+// 2. 생성된 태그를 랜더링 함수
