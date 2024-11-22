57e13097915be58149c63fb1877095c9231ac662
photoby64 <photoby64@naver.com>
2024-11-22 13:44:05 +0900

    2

--- client/main.js
+++ client/main.js
@@ -1,9 +1,11 @@
 
-import { tiger } from "./lib/index.js"
+import { getNode, insertLast, tiger } from "./lib/index.js"
 
 
 
 const END_POINT = 'https://jsonplaceholder.typicode.com/users'
+
+const userCardInner = getNode('.user-card-inner');
 
 
 // const response = await fetch(END_POINT);
@@ -16,9 +18,36 @@ async function renderUserList(){
   const data = response.data;
 
   data.forEach((user) => {
-    console.log(user);
+    console.log(user.name);
+
+    // 태그만들기
+    const tag = `  <div>${user.name}</div>  `
+    const tag2 = `
+    <article class="user-card" data-index="user-1">
+          <h3 class="user-name">${user.username}</h3>
+          <div class="user-resouce-info">
+            <div>
+              <a class="user-email" href="mailto:${user.email}">${user.email}</a>
+            </div>
+            <div>
+              <a class="user-website" href="${user.website}" target="_blank" rel="noopener noreferer">${user.website}</a>
+            </div>
+          </div>
+          <button class="delete">삭제</button>
+        </article>
+    `
+
+    // 화면에 랜더링
+    insertLast(userCardInner,tag2)
+
+    // 유저 아티클 만들기
+
+
+
+
 
   })
+
 
 }
 
