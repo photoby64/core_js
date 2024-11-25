209272bc5ed9fad6c44891dee7aca3e91bc725f7
photoby64 <photoby64@naver.com>
2024-11-25 13:24:23 +0900

    post 통신

--- client/main.js
+++ client/main.js
@@ -7,21 +7,16 @@ import {
   getNode,
   insertLast, 
   changeColor,
+  clearContents,
   renderSpinner,
   renderUserCard,
   renderEmptyCard,
-  clearContents,
  } from "./lib/index.js";
 
 
 
 const END_POINT = 'http://localhost:3000/users'
-
-
 const userCardInner = getNode('.user-card-inner');
-
-
-
 
 
 
@@ -60,7 +55,7 @@ async function renderUserList(){
       x:-100,
       opacity:0,
       stagger:{
-        amount:1,
+        each:0.1,
         from:'start'
       }
     })
@@ -76,15 +71,7 @@ async function renderUserList(){
 }
 
 
-
 renderUserList()
-
-
-
-// 1. user 데이터 fetch 해주세요.
-
-
-// 2. fetch 데이터 유저의 이름만 콘솔에 출력 
 
 
 function handleDeleteCard(e){
@@ -98,13 +85,87 @@ function handleDeleteCard(e){
   tiger.delete(`${END_POINT}/${index}`).then(()=>{
     alert('삭제가 완료됐습니다.')
 
-    clearContents(userCardInner);
 
+    // userCardInner.textContent = '';
+
+    clearContents(userCardInner)
     renderUserList()
-  })
-  
 
+    
+
+  })
 
 }
 
+
+
 userCardInner.addEventListener('click',handleDeleteCard)
+
+
+
+
+const createButton = getNode('.create');
+const cancelButton = getNode('.cancel');
+const doneButton = getNode('.done');
+
+// create 버튼을 선택한다.
+// 클릭 이벤트를 바인딩한다.
+// create에 open 클래스를 추가한다.
+
+// cancel 버튼을 선택한다.
+// 클릭 이벤트를 바인딩한다.
+// create에 open 클래스를 제거한다.
+
+
+// POST 통신을 해주세요.
+
+// 1. input의 value를 가져온다.
+// 2. value를 모아서 객체를 생성
+// 3. 생성 버튼을 누르면 POST통신을 한다.
+// 4. body에 생성한 객체를 실어보낸다.
+// 5. 카드 컨텐츠 비우기
+// 6. 유저카드 리랜더링
+
+const nameField = getNode('#nameField');
+
+function handleCreate(){
+  
+  // this.classList.add('open');
+  gsap.to('.pop',{autoAlpha:1})
+  // getNode('#nameField').focus()
+  
+}
+
+function handleCancel(e){
+  e.stopPropagation();
+  // createButton.classList.remove('open');
+  gsap.to('.pop',{autoAlpha:0})
+}
+
+
+function handleDone(e){
+  e.preventDefault();
+
+  const username = getNode('#nameField').value;
+  const email = getNode('#emailField').value;
+  const website = getNode('#siteField').value;
+
+  
+  tiger.post(END_POINT,{ username, email, website })
+  .then(()=>{
+    gsap.to('.pop',{autoAlpha:0})
+    clearContents(userCardInner)
+    renderUserList();
+
+    getNode('#nameField').value = ''
+    getNode('#emailField').value = ''
+    getNode('#siteField').value = ''
+    
+  })
+  
+}
+
+
+createButton.addEventListener('click',handleCreate)
+cancelButton.addEventListener('click',handleCancel)
+doneButton.addEventListener('click',handleDone)
